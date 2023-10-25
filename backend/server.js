54e74227802cb1ff8bf35c52f7bd04163fc83158
Jack <jack.h@carbonyte.io>
2023-10-25 14:27:48 +0100

    Added Cors

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 connectDB();
 
 const port = 5000;
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use('/api/vendorModule', require('./routes/vendorRoutes'))
-app.use(errorHandler);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Define your CORS options correctly
+const corsOptions = {
+  origin: 'http://localhost:3000', // Remove the trailing slash
+};
+
+// Apply the CORS middleware with the specified options
+app.use(cors(corsOptions));
+
+app.use('/api/vendorModule', require('./routes/vendorRoutes'));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
