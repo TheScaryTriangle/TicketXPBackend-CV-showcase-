6a4c576740f153fc18bf521675ee4d3286569f79
Jack <jack.h@carbonyte.io>
2023-10-25 13:48:39 +0100

    Added Vendor Getters

--- backend/controllers/vendorModule.js
+++ backend/controllers/vendorModule.js
@@ -5,7 +5,7 @@ const vendorTable = require('../models/vendorModel')
  * @dev Used to get all the vendors from the database
  * @param {*} res 
  */
-const getVendors = asyncHanlder(async (res) => {
+const getVendors = asyncHanlder(async (req, res) => {
     const vendors = await vendorTable.find();
     res.status(200).json(vendors)
 })
@@ -26,23 +26,25 @@ const addVendor = asyncHanlder(async (req, res) => {
 })
 
 const getVendorById = asyncHanlder(async (req, res) => {
-    const vendors = await vendorTable.find();
-    res.status(200).json(vendors)
+    const vendorFromDb = await vendorTable.findById(req.body.id);
+    res.status(200).json(vendorFromDb);
 })
 
 const updateVendor = asyncHanlder(async (req, res) => {
-    const vendorFromDb = await vendorTable.findById(req.params.id);
+    console.log(req.body.id)
+    const vendorFromDb = await vendorTable.findById(req.body.id);
+    res.status(200).json(vendorFromDb);
 
-    if (!vendorFromDb) {
-        res.status(400);
-        throw new Error('Vendor not found');
-    }
+    // if (!vendorFromDb) {
+    //     res.status(400);
+    //     throw new Error('Vendor not found');
+    // }
 
-    const updatedVendor = await vendorTable.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    });
+    // const updatedVendor = await vendorTable.findByIdAndUpdate(req.params.id, req.body, {
+    //     new: true,
+    // });
 
-    res.status(200).json(updatedVendor);
+    // res.status(200).json(updatedVendor);
 })
 
 module.exports = {
--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getVendors,addVendor,getVendorById } = require('../controllers/vendorModule')
+const { getVendors, addVendor, getVendorById, updateVendor } = require('../controllers/vendorModule')
 
 router.route('/GetAllVendors').get(getVendors).post(getVendors)
 router.route('/AddVendor').get(addVendor).post(addVendor)
 router.route('/GetVendor').get(getVendorById).post(getVendorById)
-
+router.route('/UpdateVendor').get(updateVendor).post(updateVendor)
 
 module.exports = router;
