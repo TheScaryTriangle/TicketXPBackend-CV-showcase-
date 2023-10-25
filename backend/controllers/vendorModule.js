f733cfd3d78e39cbfdde9bf5275c4aa3bb4eb1dd
Jack <jack.h@carbonyte.io>
2023-10-25 15:04:59 +0100

    Added deleting pattern

--- backend/controllers/vendorModule.js
+++ backend/controllers/vendorModule.js
@@ -1,16 +1,17 @@
-const asyncHanlder = require('express-async-handler');
-const vendorTable = require('../models/vendorModel')
+const asyncHandler = require('express-async-handler');
+const vendorTable = require('../models/vendorModel');
+const vendorModel = require('../models/vendorModel');
 
 /**
  * @dev Used to get all the vendors from the database
  * @param {*} res 
  */
-const getVendors = asyncHanlder(async (req, res) => {
+const getVendors = asyncHandler(async (req, res) => {
     const vendors = await vendorTable.find();
     res.status(200).json(vendors)
 })
 
-const addVendor = asyncHanlder(async (req, res) => {
+const addVendor = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400);
         throw new Error('Please add text field');
@@ -25,31 +26,48 @@ const addVendor = asyncHanlder(async (req, res) => {
     })
 })
 
-const getVendorById = asyncHanlder(async (req, res) => {
+const getVendorById = asyncHandler(async (req, res) => {
     const vendorFromDb = await vendorTable.findById(req.body.id);
     res.status(200).json(vendorFromDb);
 })
 
-const updateVendor = asyncHanlder(async (req, res) => {
+const updateVendor = asyncHandler(async (req, res) => {
     console.log(req.body.id)
     const vendorFromDb = await vendorTable.findById(req.body.id);
     res.status(200).json(vendorFromDb);
 
-    // if (!vendorFromDb) {
-    //     res.status(400);
-    //     throw new Error('Vendor not found');
-    // }
+    if (!vendorFromDb) {
+        res.status(400);
+        throw new Error('Vendor not found');
+    }
 
-    // const updatedVendor = await vendorTable.findByIdAndUpdate(req.params.id, req.body, {
-    //     new: true,
-    // });
+    const updatedVendor = await vendorTable.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
 
-    // res.status(200).json(updatedVendor);
+    res.status(200).json(updatedVendor);
 })
+
+const deleteVendor = asyncHandler(async (req, res) => {
+    const vendorId = req.body.id; // Assuming the ID is in the URL parameters
+    try {
+        const deletedVendor = await vendorTable.findByIdAndDelete(vendorId);
+
+        if (!deletedVendor) {
+            res.status(404).json({ message: 'Vendor not found' });
+        } else {
+            res.status(200).json({ message: 'Vendor deleted successfully' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting the vendor' });
+    }
+});
+
 
 module.exports = {
     getVendors,
     addVendor,
     getVendorById,
-    updateVendor
+    updateVendor,
+    deleteVendor
 }
--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getVendors, addVendor, getVendorById, updateVendor } = require('../controllers/vendorModule')
+const { getVendors, addVendor, getVendorById, updateVendor, deleteVendor } = require('../controllers/vendorModule')
 
 router.route('/GetAllVendors').get(getVendors).post(getVendors)
 router.route('/AddVendor').get(addVendor).post(addVendor)
 router.route('/GetVendor').get(getVendorById).post(getVendorById)
 router.route('/UpdateVendor').get(updateVendor).post(updateVendor)
+router.route('/DeleteVendor').get(deleteVendor).post(deleteVendor).delete(deleteVendor)
 
 module.exports = router;
