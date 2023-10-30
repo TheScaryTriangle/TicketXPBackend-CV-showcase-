26b5447c3a3271f33c4129fa118463eb5d6f5949
Jack <jack.h@carbonyte.io>
2023-10-30 18:08:09 +0000

    Added delete event

--- backend/controllers/ticketModule.js
+++ backend/controllers/ticketModule.js
@@ -22,7 +22,23 @@ const addEvent = asyncHandler(async (req, res) => {
     }
 })
 
+const deleteEvent = asyncHandler(async (req, res) => {
+    const eventId = req.body.id; // Assuming the ID is in the URL parameters
+    try {
+        const deletedEvent = await ticketModel.findByIdAndDelete(eventId);
+
+        if (!deletedEvent) {
+            res.status(404).json({ message: 'Event not found' });
+        } else {
+            res.status(200).json({ message: 'Event deleted successfully' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting the event' });
+    }
+})
+
 module.exports = {
     getAllEvents,
-    addEvent
+    addEvent,
+    deleteEvent
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addEvent, getAllEvents } = require('../controllers/ticketModule')
+const { addEvent, getAllEvents, deleteEvent } = require('../controllers/ticketModule')
 
 router.route('/GetAllEvents').get(getAllEvents).post(getAllEvents)
 router.route('/AddEvent').get(addEvent).post(addEvent)
+router.route('/DeleteEvent').get(deleteEvent).post(deleteEvent).delete(deleteEvent)
 
 module.exports = router;
